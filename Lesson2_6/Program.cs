cb6c92073820d962481e9c3e08699ac6211792c9
Жаслан <@DESKTOP-16APJ8S>
2021-08-20 22:10:43 +0500

    4

---
+++ Lesson2_6/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Lesson2_6
+{
+    class Program
+        /* Написать программу подсчета количества «хороших» чисел в диапазоне от 1 до 1 000 000 000. 
+         * «Хорошим» называется число, которое делится на сумму своих цифр.
+         * Реализовать подсчёт времени выполнения программы, используя структуру DateTime.
+         * */
+    {
+        static void Main(string[] args)
+        {
+            static int sumc(int n)
+            {
+                int s=0;
+                while (n!=0)
+                    {
+                    s = s+ n % 10;
+                    n =n/ 10;
+                    }
+                return s;
+                    
+            }
+            int k = 0;
+            DateTime start = DateTime.Now;
+            for (int i = 1; i <= Math.Pow(10,9); i++)
+            {
+                if (i % sumc(i) == 0)
+                    k++;
+            }
+            Console.WriteLine(DateTime.Now - start);
+
+            Console.WriteLine(k);
+        }
+    }
+}
